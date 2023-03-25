bcc54cac3f60f7c9cfbd149dd4053639bb483d6d
sabta <alencarsabta@gmail.com>
2023-03-25 08:57:01 -0400

    fix: not found error

--- src/errors/not-found-error.ts
+++ src/errors/not-found-error.ts
@@ -2,7 +2,7 @@ import { ApplicationError } from "@/protocols";
 
 export function notFoundError(): ApplicationError {
   return {
-    name: "NotFoundError",
+    name: "notFoundError",
     message: "No result for this search!",
   };
 }
